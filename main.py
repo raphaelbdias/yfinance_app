6a48e66b20fadda253fa30ba1d8acf6f6a7234a8
Raphael B. Dias <86736451+raphaelbdias@users.noreply.github.com>
2024-03-25 01:06:25 +0000

    chnages

--- main.py
+++ main.py
@@ -1,55 +1,39 @@
 import streamlit as st
 import yfinance as yf
-import plotly.graph_objects as go
-import json
 from datetime import datetime, timedelta
+import plotly.graph_objects as go
 
 def main():
     st.title('Stock Analysis')
 
     option = st.selectbox('Select Stock', ['Microsoft', 'Apple'])
 
-    graphJSON = ''
+    start_date = st.date_input('Start Date', datetime.today() - timedelta(days=30))
+    end_date = st.date_input('End Date', datetime.today())
 
-    if option == 'Microsoft':
-        d_30 = datetime.today() - timedelta(days=30)
-        msft = yf.Ticker("MSFT")
-        df = msft.history(start=d_30).reset_index()
-        fig = go.Figure(data=[
-            go.Candlestick(x=df['Date'],
-                           open=df['Open'],
-                           high=df['High'],
-                           low=df['Low'],
-                           close=df['Close'])
-        ])
-        fig.update_layout(
-            xaxis_rangeslider_visible=False,
-            title_text='Microsoft Stock Analysis for the last 30 days',
-            yaxis_title='MSFT Stock',
-            xaxis_title='Dates'
-        )
-        graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    if start_date < end_date:
+        if option == 'Microsoft':
+            msft = yf.Ticker("MSFT")
+            df = msft.history(start=start_date, end=end_date).reset_index()
 
-    elif option == 'Apple':
-        d_30 = datetime.today() - timedelta(days=30)
-        apple = yf.Ticker("AAPL")
-        df = apple.history(start=d_30).reset_index()
-        fig = go.Figure(data=[
-            go.Candlestick(x=df['Date'],
-                           open=df['Open'],
-                           high=df['High'],
-                           low=df['Low'],
-                           close=df['Close'])
-        ])
-        fig.update_layout(
-            xaxis_rangeslider_visible=False,
-            title_text='Apple Stock Analysis for the last 30 days',
-            yaxis_title='AAPL Stock',
-            xaxis_title='Dates'
-        )
-        graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        elif option == 'Apple':
+            apple = yf.Ticker("AAPL")
+            df = apple.history(start=start_date, end=end_date).reset_index()
 
-    st.plotly_chart(fig)
+        fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                                             open=df['Open'],
+                                             high=df['High'],
+                                             low=df['Low'],
+                                             close=df['Close'])])
+
+        fig.update_layout(xaxis_rangeslider_visible=False,
+                          title=f'{option} Stock Analysis from {start_date} to {end_date}',
+                          yaxis_title=f'{option} Stock',
+                          xaxis_title='Dates')
+
+        st.plotly_chart(fig)
+    else:
+        st.error('Error: End date must fall after start date.')
 
 if __name__ == '__main__':
     main()
